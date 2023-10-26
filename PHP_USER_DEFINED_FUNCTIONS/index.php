0ce7b2986497c47f9775e8638760525bd7d83fc9
Keyan Andy Delgado <96453690+2ez4Keyan@users.noreply.github.com>
2023-10-26 23:04:44 +0800

    USER_DEFINED_FUNCTIONS

---
+++ PHP_USER_DEFINED_FUNCTIONS/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP FUNCTIONS | KAD-F</title>
+</head>
+
+<body>
+    <?php
+
+    /*
+    //STRING & SUBSTRING FUNCTIONS
+    $string = "Hello World!";
+    echo strpos($string, "Wo");
+    echo str_replace("World", "Daniel", $string);
+    echo strtolower($string);
+    echo strtoupper($string);
+    echo substr($string, 2, 2);
+    echo explode(" ", $string);
+    */
+
+    /*
+    //MATH FUNCTIONS
+    $num = -5.5;
+    echo abs($num);
+    echo round($num);
+    echo pow(2, 3);
+    echo sqrt(2);
+    echo rand(1, 100);
+    */
+
+    /*
+    //ARRAY FUNCTIONS
+    $array = ["apple", "banana", "orange"];
+    echo count($array);
+    echo is_array($array);
+    echo array_push($array, "kiwi");
+    echo array_pop($array);
+    print_r(array_reverse($array));
+    $array2 = ["grapes"];
+    print_r(array_merge($array, $array2));
+    */
+
+    /*
+    //TIMESTAMP FUNCTIONS & DATETIME FUNCTIONS
+    echo date("Y-m-d H:i:s");
+    $date = "2023-04-11 12:00:00";
+    echo strtotime($date);
+    echo time()
+    */
+    ?>
+</body>
+
+</html>
