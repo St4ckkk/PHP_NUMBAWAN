9718ef7e6e013f450631b20a817118c882aa3860
Keyan Andy Delgado <96453690+2ez4Keyan@users.noreply.github.com>
2023-10-27 11:16:08 +0800

    PHP LOOPS

---
+++ PHP_LOOPS/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP LOOPS | KAD-F</title>
+</head>
+
+<body>
+    <?php
+
+    /*
+    // FOR LOOP
+    for ($i = 0; $i <= 5; $i++) {
+        echo "The value of i is: " . $i . "<br>";
+    }
+
+    // WHILE LOOP
+    $a = 5;
+    while ($a <= 10) {
+        echo "Value of a : " . $a++ . "<br>";
+    }
+    */
+
+    /*
+    // DO WHILE LOPP
+    $x = 0;
+    do {
+        echo $x++;
+    } while ($x <= 10);
+    */
+
+    /*
+    // FOR EACH LOOP
+    // INDEX ARRAY
+    $fruits = array("lemon", "orange", "banana");
+    foreach ($fruits as $fruit) {
+        echo "$fruit<br>";
+    }
+    */
+
+    /*
+    // ASSOCIATIVE ARRAY
+    $students = array(
+        "John Doe" => 23456789,
+        "Jane Smith" => 12345678,
+        "Bob Johnson" => 98765432
+    );
+
+    foreach ($students as $students => $number) {
+        echo "Name: $students Number: $number<br>";
+    }
+    */
+    ?>
+</body>
+
+</html>
